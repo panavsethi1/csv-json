8f254867cf6e8ed1980d3fa4714408afef2152d8
PanavSethi <panav.sethi.delhi@gmail.com>
2020-09-18 00:47:40 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,102 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import XLSX from "xlsx";
+import CsvDownload from "react-json-to-csv";
+
+console.log(XLSX);
 
 function App() {
+  const [excelToJson, setExcelToJson] = useState([]);
+  const [jsonToExcel, setJsonToExcel] = useState();
+  const [csvName, setCsvName] = useState("");
+  let inputJson;
+  let selectedFile;
+
+  const handleFileUpload = (event) => {
+    selectedFile = event.target.files[0];
+  };
+
+  const handleFileConvert = (event) => {
+    if (selectedFile) {
+      let fileReader = new FileReader();
+      fileReader.readAsBinaryString(selectedFile);
+      fileReader.onload = (event) => {
+        let data = event.target.result;
+        let workbook = XLSX.read(data, { type: "binary" });
+        workbook.SheetNames.forEach((sheet) => {
+          setExcelToJson((prev) => [
+            ...prev,
+            XLSX.utils.sheet_to_json(workbook.Sheets[sheet]),
+          ]);
+        });
+      };
+    }
+  };
+
+  const handleJsonChange = (event) => {
+    inputJson = event.target.files[0];
+  };
+
+  const handleJsonConvert = () => {
+    if (inputJson) {
+      let inputJsonName = String(inputJson.name);
+      let ext = Number(inputJsonName.indexOf("."));
+      setCsvName(`${inputJsonName.substring(0, ext)}.csv`);
+      let fileReader = new FileReader();
+      fileReader.readAsBinaryString(inputJson);
+      fileReader.onload = (event) => {
+        setJsonToExcel(JSON.parse(event.target.result));
+      };
+    }
+  };
+  if (jsonToExcel) {
+    console.log(Array.isArray(jsonToExcel));
+  }
+  const jsonString = JSON.stringify(excelToJson);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container" style={{ textAlign: "center" }}>
+        <h3>Excel to JSON</h3>
+        <input
+          id="xlsFile"
+          className="form-group"
+          onChange={handleFileUpload}
+          type="file"
+          accept=".xls, .xlsx, .csv"
+        ></input>
+        <button className="btn btn-lg btn-primary" onClick={handleFileConvert}>
+          Convert
+        </button>
+        {jsonString === "[]" ? null : (
+          <div>
+            <h4>Your data in JSON:</h4>
+            <p>{jsonString}</p>
+          </div>
+        )}
+        <h3>JSON to Excel</h3>
+        <input
+          className="form-group"
+          onChange={handleJsonChange}
+          type="file"
+          accept=".json, .js"
+        ></input>
+        <button className="btn btn-lg btn-primary" onClick={handleJsonConvert}>
+          Convert
+        </button>{" "}
+        <br />
+        <br />
+        {jsonToExcel !== undefined ? (
+          Array.isArray(jsonToExcel) ? (
+            <CsvDownload data={jsonToExcel} filename={csvName}>
+              Download File
+            </CsvDownload>
+          ) : (
+            <CsvDownload data={[jsonToExcel]} filename={csvName}>
+              Download File
+            </CsvDownload>
+          )
+        ) : null}
+      </div>
     </div>
   );
 }
